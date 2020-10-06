f2de728d5ea5f03e3c258c55abd3582cc456f6ac
DonaLd-D <57483340+DonaLd-D@users.noreply.github.com>
2020-10-06 21:50:33 +0800

    spring aop

---
+++ src/main/java/com/imooc/spring/aop/service/EmployeeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.imooc.spring.aop.service;
+
+public class EmployeeServiceImpl implements EmployeeService{
+
+    public void createEmployee() {
+        System.out.println("执行创建员工业务逻辑");
+    }
+}
---
+++ src/main/java/com/imooc/spring/aop/service/UserService.java
@@ -0,0 +1,6 @@
+package com.imooc.spring.aop.service;
+
+public interface UserService {
+    public void createUser();
+
+}
